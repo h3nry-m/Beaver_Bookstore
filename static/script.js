274632cdfdb79a5cc2b6c2b25cce9455fdb97db7
Henry Mu <muh@flip1.engr.oregonstate.edu>
2022-05-10 19:14:48 -0700

    Added a Hide All button when Show All is clicked

--- static/script.js
+++ static/script.js
@@ -18,7 +18,8 @@ function showSection(section){
         document.getElementById('create').style.display ='block';
         document.getElementById('update').style.display = 'block';
         document.getElementById('delete').style.display = 'block';
-
+        document.getElementById('hide').style.display = 'block';
+        document.getElementById('all').style.display = 'none';
     }
     else if (section == 'cancel'){
         document.getElementById('browse').style.display = 'block';
@@ -33,10 +34,19 @@ function showSection(section){
         document.getElementById('update').style.display = 'none';
         document.getElementById('delete').style.display = 'block';
     }
+    else if (section =='hide'){
+        document.getElementById('browse').style.display = 'block';
+        document.getElementById('create').style.display = 'none';
+        document.getElementById('update').style.display = 'none';
+        document.getElementById('delete').style.display = 'none';
+        document.getElementById('hide').style.display = 'none';
+        document.getElementById('all').style.display = 'block';
+    }
 }
 
 function newOrder() {showSection('insert');}
 function updateOrder() {showSection('update');}
 function showAll() {showSection('all');}
+function hideAll() {showSection('hide');}
 function cancelOrder() {showSection('cancel');}
 function deleteOrder() {showSection('delete');}
