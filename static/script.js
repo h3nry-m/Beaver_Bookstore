538e5365eaa27f4fcde6e360230c4bb2786266a5
Ethan Lopez <lopezet@flip3.engr.oregonstate.edu>
2022-06-02 00:48:13 -0700

    deleted most functions that hid update and delete containers and documented remaining functions

--- static/script.js
+++ static/script.js
@@ -1,52 +1,11 @@
-function showSection(section){
-    if (section == 'insert'){
-        document.getElementById('browse').style.display = 'none';
-        document.getElementById('create').style.display ='block';
-        document.getElementById('update').style.display = 'none';
-        document.getElementById('delete').style.display = 'none';
 
-    }
-    else if (section == 'update'){
-        document.getElementById('browse').style.display = 'none';
-        document.getElementById('create').style.display ='none';
-        document.getElementById('update').style.display = 'block';
-        document.getElementById('delete').style.display = 'none';
-
-    }
-    else if (section == 'all'){
-        document.getElementById('browse').style.display = 'table';
-        document.getElementById('create').style.display ='block';
-        document.getElementById('update').style.display = 'block';
-        document.getElementById('delete').style.display = 'block';
-        document.getElementById('hide').style.display = 'block';
-        document.getElementById('all').style.display = 'none';
-    }
-    else if (section == 'cancel'){
-        document.getElementById('browse').style.display = 'table';
-        document.getElementById('create').style.display ='none';
-        document.getElementById('update').style.display = 'none';
-        document.getElementById('delete').style.display = 'none';
-
-    }
-    else if (section =='delete'){
-        document.getElementById('browse').style.display = 'none';
-        document.getElementById('create').style.display ='none';
-        document.getElementById('update').style.display = 'none';
-        document.getElementById('delete').style.display = 'block'; 
-    }
-    else if (section =='hide'){
-        document.getElementById('browse').style.display = 'table';
-        document.getElementById('create').style.display = 'none';
-        document.getElementById('update').style.display = 'none';
-        document.getElementById('delete').style.display = 'none';
-        document.getElementById('hide').style.display = 'none';
-        document.getElementById('all').style.display = 'block';
-    }
+// Modifies browse and create containers to only show Create operations for a page.
+function newOrder() {
+    document.getElementById('browse').style.display = 'none';
+    document.getElementById('create').style.display ='block';
 }
-
-function newOrder() {showSection('insert');}
-function updateOrder() {showSection('update');}
-function showAll() {showSection('all');}
-function hideAll() {showSection('hide');}
-function cancelOrder() {showSection('cancel');}
-function deleteOrder() {showSection('delete');}
+// Modifies browse and create containers to only show the main table for a page.
+function cancelOrder() {
+    document.getElementById('browse').style.display = 'table';
+    document.getElementById('create').style.display ='none';
+}
